552d50ae1a78bd21ffd394605326ab5bae869331
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-05-14 23:26:52 +0430

    working on Signup

--- Todo/views.py
+++ Todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError 
 # Create your views here.
 
 def signupUser(request):
@@ -8,7 +9,11 @@ def signupUser(request):
         return render(request, 'todo/signupUser.html', {'form':UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user.objects.create_user(request.POST['username'], password=request.POST['password1'])
-            user.save()
+            try:
+                user =User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save() 
+            except IntegrityError:
+                return render(request, 'todo/signupUser.html', {'form':UserCreationForm(), 'error': 'User name is already taken please add new one'}) 
+
         else:
-            print("Hello ")
+            return render(request, 'todo/signupUser.html', {'form':UserCreationForm(), 'error': 'Password is not match please check'})   
