07020c2b8c3236b381c830d307797649ff332ea8
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-05-14 15:58:25 +0430

    final

--- Todo/views.py
+++ Todo/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 # Create your views here.
 
 def signupUser(request):
-
-    return render(request, 'todo/signupUser.html', {'form':UserCreationForm()})
+    if request.method == 'GET':
+        return render(request, 'todo/signupUser.html', {'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            user.objects.create_user(request.POST['username'], password=request.POST['password1'])
+            user.save()
+        else:
+            print("Hello ")
