2b6b52bb12b44b0df42e676d136ff91ef1edeb41
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-05-16 01:23:39 +0430

    new

--- Todo/models.py
+++ Todo/models.py
@@ -6,6 +6,6 @@ class Todo(models.Model):
     title=models.CharField(max_length=100)
     memo=models.TextField(blank=True)
     created= models.DateTimeField(auto_now_add=True)
-    datecompleted=models.DateTimeField(null=True)
+    datecompleted=models.DateTimeField(null=True, blank=True)
     importance=models.BooleanField(default=False)
     user=models.ForeignKey(User, on_delete=models.CASCADE)
