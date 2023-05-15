14855902018ecb905d1aac5f9551dddeefd619ec
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-05-15 23:51:54 +0430

    todo model created

--- Todo/models.py
+++ Todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Todo(models.Model):
+    title=models.CharField(max_length=100)
+    memo=models.TextField(blank=True)
+    created= models.DateTimeField(auto_now_add=True)
+    datecompleted=models.DateTimeField(null=True)
+    importance=models.BooleanField(default=False)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
