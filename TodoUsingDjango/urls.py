1694bcf8f5055d2d241642fe25db88288f87e238
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-05-25 12:11:18 +0430

    Final test UX and UI

--- TodoUsingDjango/urls.py
+++ TodoUsingDjango/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('current/', views.currenttodos, name='currenttodos'),
     path ('create/', views.createtodo, name='createtodo'),
     path ('completed/', views.completedtodos, name='completedtodos'),
-    path ('todo/<int:todo_pk>', views.viewtodo, name='todoview'),
+    path ('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
     path ('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
     path ('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo')
 ]
